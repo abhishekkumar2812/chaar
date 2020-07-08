112c818eb937b9ca55f89dd16eccfb442f74db7e
abhishekkumar2812 <vibrant.abhishek@gmail.com>
2020-07-09 03:59:43 +0530

    final h

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const path = require('path');
 const items = require('./routes/api/items');
 
 const app = express();
@@ -39,6 +39,14 @@ mongoose.connect(uri, {
 //Use Routes
 
 app.use('/api/items', items)
+
+if(PerformanceObserverEntryList.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 /*
 const db = require('./config/keys').mongoURI;
 
